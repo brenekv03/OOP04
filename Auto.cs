38669827014d233ac853bc2e853afb9a7fa5e6a6
endurovojta173 <vojtechbrenek173@gmail.com>
2022-04-14 17:24:42 +0200

    Nastavení metod Get

--- Auto.cs
+++ Auto.cs
@@ -8,5 +8,34 @@ namespace OOP04
 {
     class Auto
     {
-    }
+        private string znacka;
+        private double spotrebaNa100;
+        private int ujetoCelkem;
+        private DateTime rozjezdTime;
+        private TimeSpan dobaJizdyCelkem;
+        private bool jede;
+        public string GetZnacka()
+        {
+            return znacka;
+        }
+        public double GetSpotrebaNa100()
+        {
+            return spotrebaNa100;
+        }
+        public int GetUjetoCelkem()
+        {
+            return ujetoCelkem;
+        }
+        public DateTime GetRozjezdTime()
+        {
+            return rozjezdTime;
+        }
+        public TimeSpan GetDobaJizdyCelkem()
+        {
+            return dobaJizdyCelkem;
+        }
+        public bool GetJede()
+        {
+            return jede;
+        }
 }
